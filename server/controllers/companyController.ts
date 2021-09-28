2df83169d4aaea0bf29b75b6519604585790b008
lgap <luisgerardoaguilarpadilla@gmail.com>
2021-09-27 22:15:53 -0600

    se añadio ruta para crear compañias

--- server/controllers/companyController.ts
+++ server/controllers/companyController.ts
@@ -8,7 +8,7 @@ class CompanyController {
 
     try {
       const companies = database.session.run(`MATCH (c:Company)return c`);
-      const nodes = (await companies).records[0];
+      const nodes = (await companies).records;
       
       res.json({ 
       ok:true,
@@ -24,6 +24,27 @@ class CompanyController {
   }
 
   public async create(req: Request, res: Response) {
+       const { body } = req;
+
+
+
+        const query = `CREATE (empresa: Company{companyID:apoc.create.uuid(), name: "${body.name}", address: "${body.address}", lat: "${body.lat}", lon:${body.lon}});`
+
+        await database.session.run(query).then( _ => {
+
+          console.log(_);
+
+          res.status(200).json({ 
+            ok:true,
+            msg:"Empresa creada con exito."
+            })
+        }).catch( err  => {
+            res.status(500).json({
+              ok:false,
+              msg:err
+            })
+        })
+
 
 
   }
--- server/routes/companyRoutes.ts
+++ server/routes/companyRoutes.ts
@@ -13,6 +13,7 @@ class CompanyRoutes {
 
     config(): void {
         this.router.get('/',companyController.list);
+        this.router.post('/create',companyController.create);
         
         // this.router.get('/:id',productController.getOne);
         // this.router.post('/',productController.create);
